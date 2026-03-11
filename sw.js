4d416dcab5548ebad2e97cc144506685700cb45c
Marbin Alarcon <marbinalarcon9@gmail.com>
2026-03-11 07:25:05 -0500

    blog y paginas nuevas

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
                Network First para imágenes remotas
    ============================================================ */
 
-const CACHE_NAME    = 'ink-master-v3';
+const CACHE_NAME    = 'ink-master-v4';
 const STATIC_ASSETS = [
     './',
     './index.html',
